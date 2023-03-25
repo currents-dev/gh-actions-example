14dd37b1120d545d6c562cecef6b91b32f700c1d
Andrew Goldis <agoldis@gmail.com>
2023-03-25 00:27:23 -0700

    Use todo for tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,11 @@ import { cloudPlugin } from "cypress-cloud/plugin";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://en.wikipedia.org/",
-    specPattern: "cypress/integration/*.spec.js",
-    setupNodeEvents(on, config) {
-      cloudPlugin(on, config);
-      return config;
+    baseUrl: "https://todomvc.com/examples/vanillajs",
+    specPattern: "cypress/e2e/*.spec.js",
+    supportFile: "cypress/support/e2e.ts",
+    setupNodeEvents: (on, config) => {
+      return cloudPlugin(on, config);
     },
   },
   video: true,
