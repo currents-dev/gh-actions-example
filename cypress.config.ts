300b375e38520f395984e9d34dd3a109b63e5fa9
Miguel Langarano <miguelangarano@gmail.com>
2023-08-04 09:37:23 -0500

    test commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "cypress";
 import { cloudPlugin } from "cypress-cloud/plugin";
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "https://todomvc.com/examples/vanillajs",
-    specPattern: "cypress/e2e/*.spec.js",
-    supportFile: "cypress/support/e2e.ts",
-    setupNodeEvents: (on, config) => {
-      return cloudPlugin(on, config);
-    },
-  },
-  video: true,
-  videoUploadOnPasses: false,
+	e2e: {
+		baseUrl: "https://todomvc.com/examples/vanillajs",
+		specPattern: "cypress/e2e/*.spec.js",
+		supportFile: "cypress/support/e2e.ts",
+		setupNodeEvents: (on, config) => {
+			return cloudPlugin(on, config);
+		},
+	},
+	video: true,
+	videoUploadOnPasses: false,
 });
