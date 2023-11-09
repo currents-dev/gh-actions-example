561ece37b6757e96e2b932cf441d4ea433774bfc
Andrew Goldis <agoldis@gmail.com>
2023-11-09 01:06:26 -0800

    use custom mirror

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require("cypress");
+const { cloudPlugin } = require("cypress-cloud/plugin");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://en.wikipedia.org/",
     specPattern: "cypress/integration/*.spec.js",
+    async setupNodeEvents(on, config) {
+      return await cloudPlugin(on, config);
+    },
   },
   video: true,
   videoUploadOnPasses: false,
-  projectId: "Ij0RfK",
 });
